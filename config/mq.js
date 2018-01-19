4029ef73aee66da56ef4d09b68d653f168e4dd16
Tintu <p.tintumol@yahoo.com>
2018-01-19 11:50:24 +0530

    config

---
+++ config/mq.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports.rabbitServer = 'localhost';
+module.exports.rabbitPort = '5672';
+module.exports.rabbitHeartBeat = '60';
+module.exports.rabbitUser = 'guest';
+module.exports.rabbitPassword = 'guest';
